4a1d942b263d1caa106f0f49b4af73ec78271abc
Anthony Hurtado <anhurtado@outlook.com>
2024-09-24 16:42:30 -0400

    fix: updated prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,12 +2,12 @@ module.exports = {
   printWidth: 120,
   useTabs: false,
   tabWidth: 2,
-  trailingComma: 'es5', // add trailing commas in objects, arrays, etc.
-  semi: true, // add ; when needed
+  trailingComma: 'es5',
+  semi: true,
   singleQuote: true,
   bracketSpacing: true,
-  arrowParens: 'always', // braces even for single param in arrow functions (a) => { }
-  jsxSingleQuote: false, // "" for react props, like in html
-  bracketSameLine: false, // pretty JSX
-  endOfLine: 'lf', // 'lf' for linux, 'crlf' for windows, we need to use 'lf' for git
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  endOfLine: 'lf',
 };
