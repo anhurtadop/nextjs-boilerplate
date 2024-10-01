e4dc8f464f5028d9ab26ae9b9b8cb7899f561b25
Anthony Hurtado <anhurtado@outlook.com>
2024-10-01 14:47:23 -0400

    fix: general changes

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,10 +1,7 @@
 'use client';
-
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './index';
-
-// persistStore(store);
 
 export function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
   const storeRef = useRef(store);
