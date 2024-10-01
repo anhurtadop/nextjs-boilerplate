b1ec311f558727b9a8fec344cf70c6c774615899
Anthony Hurtado <anhurtado@outlook.com>
2024-10-01 14:44:19 -0400

    feat: updated way to import fonts

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const { i18n } = require('./next-i18next.config');
 const path = require('path');
 const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  // i18n,
   output: 'standalone',
   reactStrictMode: true,
   sassOptions: {
